48d7553a2c3f4e4be3a10aa8107e94cb69e386af
david <barnakdavid49@gmail.com>
2024-05-11 16:34:54 +0300

    p17v1

--- p17/games.py
+++ p17/games.py
@@ -1,23 +1,60 @@
-import pygame, random, time
+import pygame, random, time , sys
 
 IMAGES_PATH = 'images/imagesm/'
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 600
+IMAGES_PATH1 = 'images/'
+SCREEN_WIDTH = 18 * 40
+SCREEN_HEIGHT = 18 * 30
 FPS = 60
 
 pygame.init()
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 clock = pygame.time.Clock()
 
+class Bullet:
+    bullet = None
+    def __init__(self, x: int, y: int):
+        self.bullet = pygame.Surface((3, 5))
+        self.bullet.fill((0, 0, 50))
+
+        self.x = x
+        self.y = y
+        self.speed = 2
+
+    def move(self):
+        self.y -= self.speed
+
+    def draw(self):
+        screen.blit(self.bullet, (self.x, self.y))
+
+class Bullets:
+    bullet_list: list = []
+
+    def add(self, x: int, y: int):
+        b = Bullet(x, y)
+        self.bullet_list.append(b)
+
+    def move(self):
+        for b in self.bullet_list:
+            b.move()
+
+            if b.y < 0:
+                self.bullet_list.remove(b)
+
+            b.draw()
+
 
 class Player:
     moving: int = ''
     dt = 1
+    bullets = None
+
     def __init__(self):
         self.image = pygame.image.load(IMAGES_PATH + 'player-walk-000.png')
         self.x = (SCREEN_WIDTH / 2)
         self.y = SCREEN_HEIGHT - (self.image.get_height() + 10)
         self.speed = 5
+
+        self.bullets = Bullets()
 
     def draw(self):
         screen.blit(self.image, (self.x, self.y))
@@ -27,6 +64,7 @@ class Player:
             self.move_left()
         if self.moving == pygame.K_RIGHT:
             self.move_right()
+
 
     def move_left(self):
         if self.x > 0:
@@ -40,34 +78,51 @@ class Player:
         else:
             self.x = SCREEN_WIDTH - self.image.get_width()
 
-    def move_top(self):
-        pass
-
-    def move_doun(self):
+    def shoot(self):
         pass
 
 
-class Game:
+class Bg:
     bg_image = None
+    bg_surface = None
 
     def __init__(self):
-        self.bg_data = {}
+        self.x = 0
+        self.y = -SCREEN_HEIGHT
+        self.speed = 1
+        self.bg_surface = pygame.Surface((SCREEN_WIDTH, SCREEN_HEIGHT * 2))
+
         self.add_background()
-        self.player = Player()
-        self.dt = 1
-        self.interval = time.time()
 
     def add_background(self):
         self.bg_image = pygame.image.load(IMAGES_PATH + 'tile_0000.png')
-        self.bg_data['nx'] = int(SCREEN_WIDTH / self.bg_image.get_width()) + 1
-        self.bg_data['ny'] = int(SCREEN_HEIGHT / self.bg_image.get_height()) + 1
-        self.bg_data['w'] = self.bg_image.get_width()
-        self.bg_data['h'] = self.bg_image.get_height()
+        nx = int(SCREEN_WIDTH / self.bg_image.get_width()) + 1
+        ny = int(SCREEN_HEIGHT / self.bg_image.get_height()) + 1
+        w = self.bg_image.get_width()
+        h = self.bg_image.get_height()
+
+        for y in range(ny * 2):
+            for x in range(nx):
+                self.bg_surface.blit(self.bg_image, (w * x, h * y))
 
     def draw_background(self):
-        for y in range(self.bg_data['ny']):
-            for x in range(self.bg_data['nx']):
-                screen.blit(self.bg_image, (self.bg_data['w']*x, self.bg_data['h']*y))
+        self.y ++ self.speed
+
+        if self.y >= 0:
+            self.y = -SCREEN_HEIGHT
+
+        screen.blit(self.bg_surface, (self.x, self.y))
+
+
+class Game:
+    bg_game = None
+    game_run: bool = False
+
+    def __init__(self):
+        self.player = Player()
+        self.bg_game = Bg()
+        self.dt = 1
+        self.interval = time.time()
 
     def delta_time(self):
         clock.tick(FPS)
@@ -76,28 +131,51 @@ class Game:
 
         self.player.dt = self.dt
 
+    def menu(self):
+        im = pygame.image.load(IMAGES_PATH1 + 'bg_menu_1.jpg')
+        screen.blit(im, (0, 0))
+        font = pygame.font.SysFont('Arial', 20)
+        text = font.render('key S', True, 'blue')
+        screen.blit(text, (300, 100))
+
     def init(self):
         while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+                elif event .type == pygame.KEYDOWN:
+                    if event.key == pygame.K_s:
+                        self.run()
 
+            self.menu()
+            pygame.display.update()
 
+    def run(self):
+        self.game_run = True
+        while self.game_run:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     break
-                elif event .type == pygame.KEYDOWN:
+                elif event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_LEFT:
                         self.player.moving = pygame.K_LEFT
                     if event.key == pygame.K_RIGHT:
                         self.player.moving = pygame.K_RIGHT
-                elif event .type == pygame.KEYUP:
+                    if event.key == pygame.K_q:
+                        self.game_run = False
+                        break
+                elif event.type == pygame.KEYUP:
                     self.player.moving = 0
 
-            screen.fill('Black')
-            self.draw_background()
-            self.player.move()
-            self.player.draw()
+            if self.game_run:
+                self.bg_game.draw_background()
+                self.player.move()
+                self.player.draw()
 
-            pygame.display.update()
+                pygame.display.update()
+        # end while
 
 
 if __name__ == '__main__':
